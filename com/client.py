1065eee62f109d4e5b6cab0f667a095f6cc467fb
Will Malisch <wmalisch@uwo.ca>
2020-11-06 21:20:56 -0500

    c++ sensorReceiver Server is working, but the python script is unable to communicate with it. I can communicate with c++ via telnet, but need to debug why python isnt working

--- com/client.py
+++ com/client.py
@@ -1,12 +1,35 @@
 from socket import *
+from sense_emu import SenseHat
 import sys
 
+BUFFER_SIZE = 1024
+HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-SERVER = "127.0.1.1"
+SERVER = "127.0.0.1"
 ADDR = (SERVER, PORT)
+
 
 client = socket(AF_INET, SOCK_STREAM)
 client.connect(ADDR)
 
+def main():
 
+    # Initialize SenseHat object
+    sense = SenseHat()
+
+    try:
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect(ADDR)
+        accel = sense.get_accelerometer_raw()
+        x = accel['x']
+        y = accel['y']
+        z = accel['z']
+        
+        client_socket.send(str(x).encode())
+    except ConnectionRefusedError:
+        print('Error:  That host or port is not accepting connections.')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
